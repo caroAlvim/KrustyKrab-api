f2bc01c274ca22ae0b54f820c4bf4025e1ccd9d3
Carolina Alvim <caro.alvim@gmail.com>
2021-10-13 22:08:46 -0300

    rotas funcionando

--- server/controller/UsersController.js
+++ server/controller/UsersController.js
@@ -17,8 +17,13 @@ const getUserById = (req, res) => {
   res.status(201).send('Você esta na rota de users com id e esta realizando um get');
   // id
 };
+
+const updateUser = (req, res) => {
+  res.status(201).send('Você esta na rota de users com id e esta realizando um update');
+  // id
+};
 module.exports = {
-  usersGetAll, usersPost, usersDelete, getUserById,
+  usersGetAll, usersPost, usersDelete, getUserById, updateUser,
 };
 
 // metodos para get, put, update, delete para cada rota
--- server/routes/UsersRoutes.js
+++ server/routes/UsersRoutes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const {
-  usersGetAll, usersPost, usersDelete, getUserById,
+  usersGetAll, usersPost, usersDelete, getUserById, updateUser,
 } = require('../controller/UsersController');
 
 const router = Router();
 
 router.get('/', usersGetAll);
 router.post('/', usersPost);
+router.put('/', updateUser);
 router.get('/:id', getUserById);
 router.delete('/:id', usersDelete);
 // router.patch('/:id', getExample);
